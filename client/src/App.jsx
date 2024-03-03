71493d0a856715f182d3cb8c0c8af54d13813f84
pratik-hanmante <pratikhanmante3@gmail.com>
2024-03-03 17:57:53 +0530

    created the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+
 const App = () => {
   return (
-    <div>App</div>
+   
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route  path='/' element={<Home />} />
+        <Route  path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />}/> 
+        <Route path='/sign-up' element={<SignUp />}/> 
+        <Route path='/dashboard' element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
+  
   )
 }
 
